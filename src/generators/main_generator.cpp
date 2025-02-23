ae81da44217e0b0dd3a8b4a71a31863262b78c56
arthurbesse <arthurcaturian@gmail.com>
2025-02-23 20:33:53 +0400

    Minor fixes

--- src/generators/main_generator.cpp
+++ src/generators/main_generator.cpp
@@ -6,9 +6,13 @@
 
 using namespace std;
 
-#define llparam(name, min_value, max_value) \
+#define set_long_param(name, min_value, max_value) \
     parser.set_optional<long long> ("min_" #name, "min_" #name, min_value); \
     parser.set_optional<long long> ("max_" #name, "max_" #name, max_value);
+
+#define get_long_param(name) \
+    const auto min_##name = parser.get<long long>("min_" #name); \
+    const auto max_##name = parser.get<long long>("max_" #name);
 
 int main(int argc, char* argv[])
 {
@@ -16,11 +20,13 @@ int main(int argc, char* argv[])
     rnd.setSeed(argc, argv);
 
     cli::Parser parser(argc, argv);
+    set_long_param(a, 1, 10);
     parser.set_optional<long long>("s", "seed", 42, "Random seed");
     parser.enable_help();
     parser.run_and_exit_if_error();
+    get_long_param(a);
 
-    long long a = rnd.next(1, 10);
+    long long a = rnd.next(min_a, max_a);
 
     println(a);
 
